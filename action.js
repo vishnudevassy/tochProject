20eb056004c21a388fa27ef2d1145ba115d6b39c
vishnudevassy <84307137+vishnudevassy@users.noreply.github.com>
2021-05-19 21:07:37 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function b11()
+{
+document.body.style.background="purple"
+}
+function b22()
+{
+    document.body.style.background="green"  
+}
+function b33()
+{
+    document.body.style.background="yellow"   
+}
